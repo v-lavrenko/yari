15e59d7d7833ed5501f5776ebe5b42eded2112b6
Victor Lavrenko <vlavrenko@humanlongevity.com>
2016-10-18 16:59:36 +0000

    added print_vec_json

--- matrix.h
+++ matrix.h
@@ -97,6 +97,7 @@ void print_vec_svm (ix_t *vec, hash_t *ids, char *class, char *svm) ;
 void print_vec_csv (ix_t *vec, uint ncols, char *fmt) ;
 void print_vec_txt (ix_t*vec, hash_t *ids, char *vec_id, int xml) ;
 void print_vec_top (ix_t *vec, hash_t *dict, uint top) ;
+void print_vec_json (ix_t *vec, hash_t *ids, char *vec_id, char *fmt) ;
 
 void transpose_mtx   (coll_t *rows, coll_t *cols) ;
 void transpose_mtx_1 (coll_t *rows, coll_t *cols) ;
