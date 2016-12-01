4d9ae8eb622566972c42a827e4c43707f189fa7f
Victor Lavrenko <vlavrenko@humanlongevity.com>
2016-12-01 10:08:44 +0000

    adds hash2hash header

--- hash.h
+++ hash.h
@@ -50,6 +50,7 @@ uint has_key (hash_t *h, char *key) ;
 uint id2id (hash_t *src, uint id, hash_t *trg) ;
 uint *keys2ids (hash_t *h, char **keys) ; // batch version of key2id
 char **hash_keys (char *path) ; // list all keys in a hashtable
+uint *hash2hash (char *src, char *trg, char *access) ; // map ids: src -> trg 
 
 //unsigned keyIn (hash_t *t, char *key) ;
 //hash_t *hnew (unsigned num_els, char *path) ;
