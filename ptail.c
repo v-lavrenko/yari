fcef59886070eaffbb3c8a0fddbac8d4a3c1d1dc
Victor Lavrenko <v.lavrenko@gmail.com>
2019-10-08 19:51:42 -0700

    fix missing last line

--- ptail.c
+++ ptail.c
@@ -13,15 +13,17 @@ double getprm (char *params, char *name, double def) {
 
 int main (int argc, char *argv[]) {
   char *BUF = NULL;
-  size_t N = 0;
+  size_t N = 0, n = 0;
   double step = getprm(arg(1),"p=",2);
   unsigned int seed = getprm(arg(1),"s=",time(0));
   unsigned int thresh = RAND_MAX; //1<<31;
   srandom(seed);
-  while (getline(&BUF, &N, stdin) > 0) 
+  while (!feof(stdin)) {
+    n = getline(&BUF, &N, stdin);
     if (random() < thresh) {
       fputs(BUF,stdout);
       thresh /= step;
     }
-  fputs(BUF,stdout);
+  }
+  fputs(BUF,stdout); // always print last line
 }
