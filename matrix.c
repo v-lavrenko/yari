df4003995433708334f8bb164607df567e32c4ce
Victor Lavrenko <v.lavrenko@gmail.com>
2020-12-14 21:27:11 +0000

    increase default token size to 50

--- matrix.c
+++ matrix.c
@@ -847,7 +847,7 @@ ix_t *parse_vec_txt (char *str, char **id, hash_t *ids, char *prm) { // thread-u
     cgrams (str, gram, gram_hi, step, buf, 1<<26); // character n-grams
     str = buf; ws = " ";
   }
-  char **toks = str2toks (str, ws, 30); // tokens at most 30 chars
+  char **toks = str2toks (str, ws, 50); // tokens at most 50 chars
   if (stemmer) stem_toks (toks, stemmer);
   if (stop)    stop_toks (toks);
   char **pairs = strstr (prm,"w=") ? toks2pairs (toks,prm) : NULL;
