790f63a3aa6aa8f917368f482991fdefe2e7e671
Victor Lavrenko <v.lavrenko@gmail.com>
2021-01-20 14:18:24 -0800

    xtime -bench ... compares speed of time(), ftime(), mstime()

--- xtime.c
+++ xtime.c
@@ -5,9 +5,24 @@
 #include "timeutil.h"
 #include "textutil.h"
 
+int benchmark_time() {
+  uint n; double t0, t;
+  t = t0 = ftime();
+  for (n = 0; t - t0 < 5; ++n) t = ftime();
+  fprintf(stderr,"%d ftime() in %.3fs\n", n, t-t0);
+  t = t0 = mstime();
+  for (n = 0; t - t0 < 5000; ++n) t = mstime();
+  fprintf(stderr,"%d mstime() in %.3fs\n", n, (t-t0)/1E3);
+  t = t0 = time(0);
+  for (n = 0; t - t0 < 5; ++n) t = time(0);
+  fprintf(stderr,"%d time() in %.3fs\n", n, t-t0);  
+  return 0;
+}
+
 int main (int n, char *A[]) {
+  if (n>1 && !strcmp(A[1], "-bench")) return benchmark_time();	   
   char *_col = "1", *ifmt = "%s", *ofmt = "%F,%T";  int i;
-  for (i=1; i<n; ++i) {
+  for (i=1; i<n; ++i) {    
     if (!strncmp(A[i],"-f",2)) _col = A[i][2] ? A[i]+2 : ++i<n ? A[i] : _col;
     if (!strncmp(A[i],"-i",2)) ifmt = A[i][2] ? A[i]+2 : ++i<n ? A[i] : ifmt;
     if (!strncmp(A[i],"-o",2)) ofmt = A[i][2] ? A[i]+2 : ++i<n ? A[i] : ofmt;
