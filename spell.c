f8c6b8eb061c48458948b5d0d9639269bcb27a2e
Victor Lavrenko <v.lavrenko@gmail.com>
2021-01-29 18:29:46 -0800

    adds: try_split(), try_fuse() + usage

--- spell.c
+++ spell.c
@@ -98,13 +98,40 @@ int best_edit (char *word, uint nedits, hash_t *known, float *score, uint *best)
   return 0;
 }
 
-uint spell_check1 (char *word, hash_t *known, float *df) {
-  
-  
-  
-  
+// aminoacid -> {i:amino, j:acid, x: MIN(F[amino],F[acid])}
+jix_t try_split (char *word, hash_t *known, float *score) {
+  jix_t best = {0, 0, 0};
+  int n = strlen(word), k;
+  for (k = 1; k < n; ++k) {
+    char *wi = strndup(word,k), *wj = strndup(word+k,n-k);
+    uint i = has_key(known,wi), j = has_key(known,wj);
+    float x = (i && j) ? MIN(score[i],score[j]) : 0;
+    if (x > best.x) best = (jix_t) {j, i, x};
+    free(wi); free(wj);
+  }
+  return best;
 }
 
+// amino acid ic -> amino acidic
+uint try_fuse (char *w1, char *w2, char *w3, hash_t *known, float *score) {
+  int n1 = strlen(w1), n2 = strlen(w2), n3 = strlen(w3);
+  char *w12 = calloc(n1+n2+1,1); strcat(w12,w1); strcat(w12,w2);
+  char *w23 = calloc(n2+n3+1,1); strcat(w23,w2); strcat(w23,w3);
+  uint i12 = has_key(known,w12); float x12 = i12 ? score[i12] : 0;
+  uint i23 = has_key(known,w23); float x23 = i23 ? score[i23] : 0;
+  free(w12); free(w23);
+  return x12 > x23 ? i12 : x23 > 0 ? i23 : 0;
+}
+
+uint spell_check (char *word, hash_t *known, float *F) {
+  uint i0 = has_key (known, word), i1=0, i2=0;
+  if (i0 && F[i0] > 5) return i0;
+  best_edit (word, 1, known, F, &i1); 
+  if (i1 && F[i1] > F[i0]) return i1;
+  best_edit (word, 2, known, F, &i2);
+  if (i2 && F[i2] > F[i0]) return i2;
+  return 0;
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
@@ -149,7 +176,7 @@ int do_spell_3 (char *word, uint nedits, hash_t *known, float *cf) {
   t0=ustime(); best_edit (word, nedits, known, cf, &best);
   t1=ustime(); char *corr = best ? id2key(known,best) : "";
   printf ("%s %s:%.0f\n", word, corr, cf[best]);
-  printf ("besr: %ld\n", t1-t0);
+  printf ("best: %ld\n", t1-t0);
   return 0;
 }
 
@@ -167,8 +194,53 @@ int do_spell_12 (char *word, uint n, char *_H, char *_M) {
   return 0;
 }
 
+int do_fuse (char *w1, char *w2, char *w3, char *_H, char *_F) {
+  hash_t *H = open_hash (_H,"r");
+  coll_t *M = open_coll (_F, "r+");
+  ix_t *_row = get_vec_ro (M,1);
+  float *F = vec2full (_row, num_cols(M), 0);
+  free_coll(M);
+  uint id = try_fuse (w1, w2, w3, H, F);
+  uint i1 = has_key(H,w1), i2 = has_key(H,w2), i3 = has_key(H,w3);
+  char *word = id ? id2key(H,id) : "N/A";
+  printf("%s:%.0f <- %s:%.0f %s:%.0f %s:%.0f\n",
+	 word, F[id], w1, F[i1], w2, F[i2], w3, F[i3]);
+  free_vec(F);
+  free_hash(H);
+  return 0;
+}
+
+int do_split (char *word, char *_H, char *_F) {
+  hash_t *H = open_hash (_H,"r");
+  coll_t *M = open_coll (_F, "r+");
+  ix_t *_row = get_vec_ro (M,1);
+  float *F = vec2full (_row, num_cols(M), 0);
+  free_coll(M);
+  jix_t s = try_split (word, H, F);
+  char *w1 = s.i ? id2key(H,s.i) : "N/A";
+  char *w2 = s.j ? id2key(H,s.j) : "N/A";
+  uint id = has_key(H,word);
+  printf("%s:%.0f -> %s:%.0f %s:%.0f\n", word, F[id], w1, F[s.i], w2, F[s.j]);
+  free_vec(F);
+  free_hash(H);
+  return 0;
+}
+
+#define arg(i) ((i < argc) ? A[i] : NULL)
+#define a(i) ((i < argc) ? A[i] : "")
+
 int main (int argc, char *A[]) {
-  if (argc == 5) return do_spell_12 (A[1], atoi(A[2]), A[3], A[4]);
+  if (argc < 2) {
+    fprintf (stderr, 
+	     "usage: spell -spell remdesivir 1 WORD WORD_CF\n"
+	     "       spell -fuse  amino acid ic WORD WORD_CF\n"
+	     "       spell -split coronavirus WORD WORD_CF\n"
+	     );
+    return 1;
+  }
+  if (!strcmp(a(1),"-fuse"))  return do_fuse (a(2), a(3), a(4), a(5), a(6));
+  if (!strcmp(a(1),"-split")) return do_split (a(2), a(3), a(4));
+  if (!strcmp(a(1),"-spell")) return do_spell_12 (a(2), atoi(a(3)), a(4), a(5));
   if (argc == 3) {
     hash_t *H = open_hash (0,0);
     double t0 = 1E3 * ftime();
--- spell.h
+++ spell.h
@@ -11,3 +11,8 @@ int known_edits (char *word, uint nedits, hash_t *known, float *score, ix_t **re
 // *best = id of edit that has highest score
 int best_edit (char *word, uint nedits, hash_t *known, float *score, uint *best) ;
 
+// aminoacid -> {i:amino, j:acid, x: MIN(F[amino],F[acid])}
+jix_t try_split (char *word, hash_t *known, float *score) ;
+
+// amino acid ic -> amino acidic
+uint try_fuse (char *w1, char *w2, char *w3, hash_t *known, float *score) ;
