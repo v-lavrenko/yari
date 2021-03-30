d0a61c9211be890bfae57511a11e8a6af1fe24c6
Victor Lavrenko <v.lavrenko@gmail.com>
2021-03-30 11:25:30 -0700

    bit operations -> bitvec.h

--- bloom.c
+++ bloom.c
@@ -21,16 +21,11 @@
 
 // simple randomized helpers
 #include "vector.h"
+#include "bitvec.h"
 
 #define loginc(n) (n + ((random() / 2147483647) < (1/n)))
 
 #define loginc(n) (n + (log(n) + log(random()) <  log(2147483647)))
-
-#define getbit(B,i) (B[i>>5] &   (1 << (i & 31)))
-#define setbit(B,i) (B[i>>5] |=  (1 << (i & 31)))
-#define clrbit(B,i) (B[i>>5] &= ~(1 << (i & 31)))
-
-uint *bitvec(uint N) { return new_vec (N/32, sizeof(uint)); }
 
 
 void bloom_add (uint *B, char *el) {
