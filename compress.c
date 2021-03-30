5a38316ae17d555549ea87f37745585a09dd55a8
Victor Lavrenko <v.lavrenko@gmail.com>
2021-03-30 11:26:17 -0700

    major rewrites

--- compress.c
+++ compress.c
@@ -1,4 +1,6 @@
 #include "hash.h"
+#include "bitvec.h"
+#include "timeutil.h"
 
 /* -------------------- ideas -------------------- 
    
@@ -31,7 +33,7 @@ void delta_decode (uint *V) {
 }
 
 
-// ---------------------------------------- v-byte encoding
+// ---------------------------------------- v-byte / varint encoding
 
 //#define last_byte(char b) b & 
 
@@ -43,7 +45,7 @@ void reverse (char *beg, char *end) {
 }
 
 // push vbytes of U into buffer, return EOB
-char *push_vbyte (uint U, char *buf) {
+char *push_vbytes (uint U, char *buf) {
   char *end = buf;
   do {
     *end++ = U & 127; // 7 lowest bits (vbyte) -> end of buffer
@@ -55,7 +57,7 @@ char *push_vbyte (uint U, char *buf) {
 }
 
 // pop vbytes from buf, reconstruct U, return EOB
-char *pop_vbyte (uint *U, char *buf) {
+char *pop_vbytes (uint *U, char *buf) {
   *U = 0;
   do {
     *U <<= 7; // make space for 7 bits at the end of U
@@ -66,43 +68,140 @@ char *pop_vbyte (uint *U, char *buf) {
 }
 
 char *vbyte_encode (uint *U) {
-  char *B = new_vec (4*len(U), sizeof(char)), *buf = B;
+  char *B = new_vec (5*len(U), sizeof(char)), *buf = B;
   uint *u = U-1, *end = U+len(U);
-  while (++u < end) buf = push_vbyte (*u, buf);
+  while (++u < end) buf = push_vbytes (*u, buf);
   len(B) = buf - B;
   return B;
 }
 
 uint *vbyte_decode (char *B) {
-  uint *U = new_vec (0, sizeof(uint)), u;
+  uint *U = new_vec (0, sizeof(uint)), u = 0;
   char *b = B, *end = B+len(B);
   while (b < end) {
-    b = pop_vbyte (&u, b);
+    b = pop_vbytes (&u, b);
     U = append_vec (U, &u);
   }
-  return U;  
+  return U;
+}
+
+// Little Endian: 0x11223344 -> {44,33,22,11} in memory
+
+// ---------------------------------------- https://www.slideshare.net/profyclub_ru/sphinx-82002790
+
+uchar *push_msint (uint U, uchar *B) {
+  uint *u = (uint*) (B+1);
+  if (U <= 251)      { *B = U; return B+1; }
+  if (U <= 0xff)     { *B = 252; *u = U & 0xff; return B+2; }
+  if (U <= 0xffff)   { *B = 253; *u = U & 0xffff; return B+3; }
+  if (U <= 0xffffff) { *B = 254; *u = U & 0xffffff; return B+4; }
+  {                    *B = 255; *u = U;              return B+5; }  
+}
+
+uchar *pop_msint (uint *U, uchar *B) {
+  uint *u = (uint*) (B+1);
+  switch (*B) { // 1st byte
+  case 252: *U = *u & 0xff;     return B+2;
+  case 253: *U = *u & 0xffff;   return B+3;
+  case 254: *U = *u & 0xffffff; return B+4;
+  case 255: *U = *u;            return B+5;
+  default:  *U = *B;            return B+1;
+  }
+}
+
+char *msint_encode (uint *U) {
+  uchar *B = new_vec (5*len(U), sizeof(char)), *buf = B;
+  uint *u = U-1, *end = U+len(U);
+  while (++u < end) buf = push_msint (*u, buf);
+  len(B) = buf - B;
+  return (char*)B;
+}
+
+uint *msint_decode (char *B) {
+  uint *U = new_vec (0, sizeof(uint)), u=0;
+  uchar *b = (uchar*)B, *end = b+len(b);
+  while (b < end) {
+    b = pop_msint (&u, b);
+    U = append_vec (U, &u);
+  }
+  return U;
 }
 
 // ---------------------------------------- Elias Gamma
 
-// prefix: z = ilog2(n) zeros ... ilog(42) = 5 -> 00000
-// suffix: n - 2^z in binary  ... 42 - 32 = 10 -> 01010
-// code: prefix1suffix        ... 42 -> 00000 1 01010
+// little endian: 0x11223344 -> {44,33,22,11} in memory
 
+// prefix: ilog2(n) zeros ... 00000 <- 5 = ilog(42)
+// suffix: n in binary    ... 101010 <- 42
+// code: prefix.suffix    ... 00000 101010 <- 42
 
-char *push_gamma (uint U, char *buf) {
-  //char *end = buf-1;
-  //uint pfx = ilog2 (U);
-  //while (++end < buf+pfx) *end = '0';
-  (void)U; return buf;  
+void show_0xb (uint u) {
+  int i = 31;
+  for (; i>=24; --i) { fputc(((u & (1<<i)) ? '1' : '0'), stdout); } fputc(' ', stdout);
+  for (; i>=16; --i) { fputc(((u & (1<<i)) ? '1' : '0'), stdout); } fputc(' ', stdout);
+  for (; i>=8; --i)  { fputc(((u & (1<<i)) ? '1' : '0'), stdout); } fputc(' ', stdout);
+  for (; i>=0; --i)  { fputc(((u & (1<<i)) ? '1' : '0'), stdout); } fputc(' ', stdout);
 }
 
-char *pop_gamma (uint *U, char *buf) {
-  (void)U; return buf;  
+void show_buf0 (char *B) ;
+
+off_t push_gamma (uint U, uchar *B, off_t beg) { // assume B[beg..] is zeroed out
+  uint i, nb = ilog2(U)+1; // how many bits in binary repr. of U
+  //printf ("gamma: %u | bits: %u | beg: %lu\n", U, nb, beg);
+  beg += nb-1; // hop over prefix: nb-1 zeros
+  for (i=0; i<nb; ++i) {
+    uint j = nb-i-1;
+    //show_0xb (U); printf (" bit [%d] -> %d\n", j, ((U & (1<<j))>0));
+    if (U & (1<<j)) {
+      //printf ("set %u -> %lu\n", j, beg+i);
+      bit_set_1(B,(beg+i));
+    }
+  }
+  return beg+nb;
+}
+  
+off_t pop_gamma (uint *_U, uchar *B, off_t beg, off_t eob) {
+  uint U = 0;
+  off_t j, i = beg;  
+  while ((i < eob) && !bit_is_1(B,i)) ++i;
+  //printf ("popgamma %lu:%lu from ", beg, eob);
+  //show_buf0((char*)B); 
+  if (i >= eob) { /* printf(" i=%lu hit EOB, stop\n", i); */ return i; }
+  uint nb = i - beg + 1; // how many bits I need
+  //printf (" expect %u bits in %lu:%lu\n", nb, i, i+nb);
+  for (j = i; j < i+nb; ++j) {
+    char bit = (bit_is_1(B,j)) > 0;
+    uint k = nb-(j-i)-1;
+    if (bit) U |= (1 << k);
+    //printf ("\tbit %lu is %d -> bit %u yields %u\n", j, bit, k, U);
+  }
+  //printf("\tfinal %u, new offset: %lu\n", U, j);
+  *_U = U;
+  return j;
 }
 
+char *gamma_encode (uint *U) {
+  uchar *B = bit_vec (65*len(U)); off_t b = 0;
+  uint *u = U-1, *end = U+len(U);
+  while (++u < end) {
+    //printf("gamma: %u -> B[%lu]\n", *u, b);
+    b = push_gamma (*u, B, b);
+  }
+  //printf("trunc: %lu bits -> %lu bytes\n", b, ((b>>3)+1));
+  len(B) = (b>>3) + 1;
+  return (char*)B;
+}
 
-
+uint *gamma_decode (char *_B) {
+  uint *U = new_vec (0, sizeof(uint)), u=0;
+  uchar *B = (uchar*)_B;
+  off_t b = 0, eob = len(B) << 3;
+  while (b < eob) {
+    b = pop_gamma (&u, B, b, eob);
+    if (b < eob) U = append_vec (U, &u);
+  }
+  return U;
+}
 
 // ---------------------------------------- counting values
 
@@ -156,23 +255,26 @@ uint *argv2vec (char *A[], int n) {
 void show_vec (uint *V, char *pre) {
   uint *v = V-1, *end = V+len(V);
   if (pre) printf ("%s ", pre);
-  while (++v < end) printf("%d ", *v);
-  printf("[%d]\n", len(V)*vesize(V));
+  while (++v < end) printf("%u ", *v);
+  printf("[%u]\n", len(V)*vesize(V));
 }
 
-#define getbit(B,i) (B & (1 << (i & 31)))
-
+#define _getbit(B,i) (B & (1 << (i & 31)))
+ 
 void show_binary (char c) {
   int i;
-  for (i=7; i>=0; --i) fputc((getbit(c,i)?'1':'0'), stdout);
+  for (i=7; i>=0; --i) fputc( ((c & (1<<i)) ? '1' : '0'), stdout);
   fputc(' ',stdout);
 }
 
-void show_buf (char *B, char *pre) {
+void show_buf0 (char *B) {
   char *b = B-1, *end = B+len(B);
+  while (++b < end) show_binary (*b);  
+}
+
+void show_buf (char *B, char *pre) {
   if (pre) printf ("%s ", pre);
-  //while (++b < end) printf("%hhx ", *b);
-  while (++b < end) show_binary (*b);
+  show_buf0(B);
   printf("[%d]\n", len(B)*vesize(B));
 }
 
@@ -198,6 +300,28 @@ int do_vbyte (int n, char *A[]) {
   return 0;
 }
 
+int do_msint (int n, char *A[]) {
+  uint *V = argv2vec (A,n);
+  show_vec (V, "original:");
+  char *B = msint_encode (V);
+  show_buf (B, "encoded:");
+  uint *D = msint_decode (B);
+  show_vec (D, "decoded:");
+  free_vec(V); free_vec(B); free_vec(D);
+  return 0;
+}
+
+int do_gamma (int n, char *A[]) {
+  uint *V = argv2vec (A,n);
+  show_vec (V, "original:");
+  char *B = gamma_encode (V);
+  show_buf (B, "encoded:");
+  uint *D = gamma_decode (B);
+  show_vec (D, "decoded:");
+  free_vec(V); free_vec(B); free_vec(D);
+  return 0;
+}
+
 int do_mtx_debug (uint i, uint *U, uint *V, char *B, char *pre) {
   printf ("row %d %s\n", i, pre);
   show_vec (U,"U:");
@@ -208,29 +332,46 @@ int do_mtx_debug (uint i, uint *U, uint *V, char *B, char *pre) {
 
 #define vsize(v) (len(v) * vesize(v) + sizeof(vec_t))
 
-int do_mtx (char *_M) {
+char *do_encode (uint *U, char a) {
+  switch (a) {
+  case 'v': return vbyte_encode(U);
+  case 'm': return msint_encode(U);
+  case 'g': return gamma_encode(U);
+  default:  return NULL;
+  }
+}
+
+uint *do_decode (char *B, char a) {
+  switch (a) {
+  case 'v': return vbyte_decode(B);
+  case 'm': return msint_decode(B);
+  case 'g': return gamma_decode(B);
+  default:  return NULL;
+  }
+}
+
+#define ENCODE gamma_encode
+#define DECODE gamma_decode
+
+int do_mtx (char *_M, char *alg) {
   coll_t *M = open_coll (_M, "r+");
   int i, n = nvecs(M);
+  double SZU = 0, SZB = 0, t0 = ftime();
   for (i = 1; i <= n; ++i) {
-    ix_t *V = get_vec_ro (M, i);
-    uint *U = ix2i (V), j;
-    char *B = vbyte_encode (U);
-    uint *BU= vbyte_decode (B);
-    delta_encode (U);
-    char *D = vbyte_encode (U);
-    uint *DU= vbyte_decode (D);
-    delta_decode (U);
-    delta_decode (DU);
-    double szU = vsize(U), szB = vsize(B), szD = vsize(D);
-    double crB = 100*szB/szU, crD = 100*szD/szU;
+    uint *U = ix2i (get_vec_ro(M,i)), *V = copy_vec(U), j;
+    delta_encode (V);
+    char *B = do_encode (V, *alg);
+    uint *BU= do_decode (B, *alg);
+    delta_decode (BU);
+    double szU = vsize(U), szB = vsize(B), crB = 100*szB/szU;
+    double CRB = 100*(SZB+=szB)/(SZU+=szU);
+    double dT = ftime() - t0, MpS = (SZB/dT)/1E6;
     assert (len(U) == len(BU));
-    assert (len(U) == len(DU));
-    for (j=0; j<len(U); ++j) {
+    for (j=0; j<len(U); ++j) 
       if (U[j] != BU[j]) return do_mtx_debug (i, U, BU, B, "B");
-      if (U[j] != DU[j]) return do_mtx_debug (i, U, DU, D, "D");
-    }
-    printf ("%4d vbyte: %.0f%% delta: %.0f%% %d\n", i, crB, crD, len(U)); fflush(stdout);
-    free_vec (U); free_vec (B); free_vec (D); free_vec (BU); free_vec (DU);
+    printf ("%d\t%d\t%s: %2.0f%% | %2.0f%% %.0f M/s\n",
+	    i, len(U), alg, crB, CRB, MpS); fflush(stdout);
+    free_vec (U); free_vec(V); free_vec (B); free_vec (BU); 
   }
   free_coll (M);
   return 0;
@@ -239,7 +380,9 @@ int do_mtx (char *_M) {
 char *usage =
   "compress -delta 1 2 3 4 5 6 7 8\n"
   "compress -vbyte 1 2 3 4 5 6 7 8\n"
-  "compress -mtx ROWSxCOLS\n"
+  "compress -msint 1 2 3 4 5 6 7 8\n"
+  "compress -gamma 1 2 3 4 5 6 7 8\n"
+  "compress -mtx ROWSxCOLS {vbyte,msint,gamma}\n"
   ;
 
 #define arg(i) ((i < argc) ? argv[i] : NULL)
@@ -249,7 +392,9 @@ int main (int argc, char *argv[]) {
   if (argc < 2) return fprintf (stderr, "%s", usage);
   if (!strcmp (a(1),"-delta")) return do_delta(argc-2,argv+2);  
   if (!strcmp (a(1),"-vbyte")) return do_vbyte(argc-2,argv+2);
-  if (!strcmp (a(1),"-mtx")) return do_mtx(arg(2));
+  if (!strcmp (a(1),"-msint")) return do_msint(argc-2,argv+2);
+  if (!strcmp (a(1),"-gamma")) return do_gamma(argc-2,argv+2);
+  if (!strcmp (a(1),"-mtx")) return do_mtx(arg(2), a(3));
   return 1;
 }
 
