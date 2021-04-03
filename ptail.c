6792d43bf9630cb6054adf60f64d3581def6c0a4
Victor Lavrenko <v.lavrenko@gmail.com>
2021-04-02 18:54:05 -0700

    fflush + n=STOP-AFTER-N-LINES

--- ptail.c
+++ ptail.c
@@ -14,18 +14,21 @@ double getprm (char *params, char *name, double def) {
 
 int main (int argc, char *argv[]) {
   char *BUF = NULL;
-  size_t N = 0;
+  size_t sz = 0;
   double step = atof(arg(1));
   if (!step) step = getprm(arg(1),"p=",2);
   unsigned int seed = getprm(arg(1),"s=",time(0));
   unsigned int thresh = RAND_MAX; //1<<31;
+  unsigned int N = getprm(arg(1),"n=",0), n=0;
   srandom(seed);
   while (!feof(stdin)) {
-    getline(&BUF, &N, stdin);
+    getline(&BUF, &sz, stdin);
     if (random() < thresh) {
       fputs(BUF,stdout);
+      fflush(stdout);
       thresh /= step;
     }
+    if (N && ++n > N) break;
   }
   fputs(BUF,stdout); // always print last line
 }
