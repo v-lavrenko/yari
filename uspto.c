852ca06baf05bcc1495eab0c810345fd4ed44ec8
Victor Lavrenko <v.lavrenko@gmail.com>
2022-11-14 18:33:28 +0000

    stub: USPTO xml parser

---
+++ uspto.c
@@ -0,0 +1,9 @@
+#include "textutil.h"
+
+char *extract_between (char *buf, char *A, char *B);
+
+int read_doc (FILE *in, char *buf, int n, char *beg, char *end) ;
+
+void uspto_to_sgml (FILE *in) {
+  
+}
